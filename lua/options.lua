ee520a273b94c4f3422e6f6517912dc5cf6ce846
Astonish <gatmacagit@gmail.com>
2024-08-12 10:18:25 +0200

    added more options

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,10 @@
--- Here put your options
+-- Here I will put my NeoVim options
+
 -- Hide Current line highlight background
 vim.o.cursorlineopt = "number"
+
+-- Enable mouse mode
+vim.opt.mouse = "a"
+
+-- Save undo history
+vim.opt.undofile = true
